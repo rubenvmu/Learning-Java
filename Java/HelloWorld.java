cbfd54b13ed8449249f7b5d97e7231389ee5d515
rvmu@rvmu ㅤ <rvmu@rvmu.es>
2024-07-27 05:28:28 +0200

    commit 2

--- Java/HelloWorld.java
+++ Java/HelloWorld.java
@@ -1,3 +1,5 @@
-public static void main(String[] args) {
-    System.err.println("Hello World!");
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.err.println("Hello World!");
+    }
 }
